1fbf7ba331510eae90777f580503355f5c53c43a
JamiesWhiteShirt <erlend@aamdal.com>
2014-12-06 10:53:36 +0100

    I don't have time for commit messages

---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateController : MonoBehaviour
+{
+	private bool open;
+	private int doorsStage;
+
+	public int openingTicks = 100;
+	public GameObject doorA;
+	private Vector3 originA;
+	public GameObject doorB;
+	private Vector3 originB;
+
+	void Start()
+	{
+		originA = doorA.transform.position;
+		originB = doorB.transform.position;
+	}
+	
+	void FixedUpdate()
+	{
+		if (open) doorsStage++;
+		else doorsStage--;
+
+		if (doorsStage > openingTicks) doorsStage = openingTicks;
+		else if (doorsStage < 0) doorsStage = 0;
+
+		doorA.transform.position = originA + new Vector3(0.0f, 2.0f * doorsStage / openingTicks, 0.0f);
+		doorB.transform.position = originB + new Vector3(0.0f, -2.0f * doorsStage / openingTicks, 0.0f);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		open = true;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float accelerationPotential = 10000.0f;
+	public float maxSpeed = 10.0f;
+	public float idleSpeedDropoff = 5000.0f;
+
+	void Start()
+	{
+		
+	}
+	
+	void FixedUpdate()
+	{
+		Vector2 acceleration = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		if (acceleration.magnitude > 1.0f)
+		{
+			acceleration.Normalize();
+		}
+
+		if (acceleration.magnitude > 0.01f)
+		{
+			//Acceleration is sufficient 
+			acceleration *= accelerationPotential;
+		}
+		else
+		{
+			//Acceleration is not sufficient, player will be brought to stop
+
+			acceleration = (-rigidbody2D.velocity.normalized / maxSpeed) * idleSpeedDropoff;
+			if (acceleration.magnitude * Time.fixedDeltaTime > rigidbody2D.velocity.magnitude)
+			{
+				acceleration = -rigidbody2D.velocity / Time.fixedDeltaTime;
+			}
+		}
+
+		rigidbody2D.AddForce(acceleration * Time.fixedDeltaTime, ForceMode2D.Impulse);
+
+		//Enforce speed limit
+		if (rigidbody2D.velocity.magnitude > maxSpeed)
+		{
+			rigidbody2D.velocity = rigidbody2D.velocity.normalized * maxSpeed;
+		}
+	}
+}
